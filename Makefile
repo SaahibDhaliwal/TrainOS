0ba518d8fab7e9e4ce5ae166653f8a06d3edf132
Saahibdeep Dhaliwal <s46dhali@uwaterloo.ca>
2025-05-16 00:42:20 -0400

    get c++ to work

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ XDIR:=/u/cs452/public/xdev
 ARCH=cortex-a72
 TRIPLE=aarch64-none-elf
 XBINDIR:=$(XDIR)/bin
-CC:=$(XBINDIR)/$(TRIPLE)-gcc
+CXX:=$(XBINDIR)/$(TRIPLE)-g++
 OBJCOPY:=$(XBINDIR)/$(TRIPLE)-objcopy
 OBJDUMP:=$(XBINDIR)/$(TRIPLE)-objdump
 
@@ -23,13 +23,13 @@ CFLAGS:=-g -pipe -static -ffreestanding -mcpu=$(ARCH) -march=armv8-a $(MMUFLAGS)
 
 # -Wl,option tells gcc to pass 'option' to the linker with commas replaced by spaces
 # doing this rather than calling the linker directly simplifies the compilation procedure
-LDFLAGS:=-Wl,-nmagic -Wl,-Tlinker.ld -Wl,--no-warn-rwx-segments -nostartfiles
+LDFLAGS:=-Wl,-nmagic -Wl,-Tlinker.ld -Wl,--no-warn-rwx-segments -nostartfiles -lstdc++ -lgcc
 
 # Source files and include dirs
-SOURCES := $(wildcard *.S) $(wildcard *.c)
+SOURCES := $(wildcard *.S) $(wildcard *.c) $(wildcard *.cpp) $(wildcard *.cc)
 # Create .o and .d files for every .c and .S (hand-written assembly) file
-OBJECTS := $(patsubst %.S, %.o, $(patsubst %.c, %.o, $(SOURCES)))
-DEPENDS := $(patsubst %.S, %.d, $(patsubst %.c, %.d, $(SOURCES)))
+OBJECTS := $(patsubst %.S, %.o, $(patsubst %.c, %.o, $(patsubst %.cpp, %.o, $(patsubst %.cc, %.o, $(SOURCES)))))
+DEPENDS := $(patsubst %.S, %.d, $(patsubst %.c, %.d, $(patsubst %.cpp, %.d, $(patsubst %.cc, %.d, $(SOURCES)))))
 
 # The first rule is the default, ie. "make", "make all" and "make iotest.img" mean the same
 all: iotest.img
@@ -41,15 +41,21 @@ iotest.img: iotest.elf
 	$(OBJCOPY) -S -O binary $< $@
 
 iotest.elf: $(OBJECTS) linker.ld
-	$(CC) $(CFLAGS) $(filter-out %.ld, $^) -o $@ $(LDFLAGS)
+	$(CXX) $(CFLAGS) $(filter-out %.ld, $^) -o $@ $(LDFLAGS)
 ifneq ($(MMU),on)
 	@$(OBJDUMP) -d $@ | grep -Fq q0 && printf "\n***** WARNING: SIMD DETECTED! *****\n\n" || true
 endif
 
 %.o: %.S Makefile
-	$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
+	$(CXX) $(CFLAGS) -MMD -MP -c $< -o $@
 
 %.o: %.c Makefile
-	$(CC) $(CFLAGS) -MMD -MP -c $< -o $@
+	$(CXX) $(CFLAGS) -MMD -MP -c $< -o $@
+
+%.o: %.cpp Makefile
+	$(CXX) $(CFLAGS) -MMD -MP -c $< -o $@
+
+%.o: %.cc Makefile
+	$(CXX) $(CFLAGS) -MMD -MP -c $< -o $@
 
 -include $(DEPENDS)
---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <string.h>
+
+#include <array>
+
+#include "rpi.h"
+
+extern "C" void setup_mmu();
+
+void run_init_array() {
+    using init_func_t = void (*)();
+    extern init_func_t __init_array_start[];
+    extern init_func_t __init_array_end[];
+
+    for (init_func_t* func = __init_array_start; func < __init_array_end; func += 1) {
+        if (*func) {
+            (*func)();
+        }
+    }
+}
+
+void initialize_bss() {
+    extern unsigned int __bss_start__;
+    extern unsigned int __bss_end__;
+
+    for (unsigned int* p = &__bss_start__; p < &__bss_end__; p += 1) {
+        *p = 0;
+    }
+}
+
+int main() {
+#if defined(MMU)
+    setup_mmu();
+#endif
+    initialize_bss();  // sets entire bss region to 0's
+    run_init_array();  // call constructors
+
+    // set up GPIO pins for both console and marklin uarts
+    gpio_init();
+    // not strictly necessary, since console is configured during boot
+    uart_config_and_enable(CONSOLE);
+    // welcome message
+    uart_puts(CONSOLE, "\r\nHello world, this is version: " __DATE__ " / " __TIME__ "\r\n\r\nPress 'q' to reboot\r\n");
+
+    unsigned int counter = 1;
+
+    // declare kernel data structures
+    // initialize( tds ); // tds is an array of TDs
+    // for(int i = 0; i < 4; i++ ) {
+    // 	active = schedule( tds );
+    // 	kerxit( active, req ); // req is a pointer to a Request
+    // 	handle( tds, req );
+    // }
+
+    // initialize user task: set up stack and "fake" context and resume
+
+    for (;;) {
+        uart_printf(CONSOLE, "PI[%u]> ", counter++);
+        for (;;) {
+            char c = uart_getc(CONSOLE);
+            uart_putc(CONSOLE, c);
+            if (c == '\r') {
+                uart_putc(CONSOLE, '\n');
+                break;
+            } else if (c == 'q' || c == 'Q') {
+                uart_puts(CONSOLE, "\r\n");
+                return 0;
+            }
+        }
+    }
+}
+
+#if !defined(MMU)
+#include <stddef.h>
+
+// define our own memset to avoid SIMD instructions emitted from the compiler
+void* memset(void* s, int c, size_t n) {
+    for (char* it = (char*)s; n > 0; --n) *it++ = c;
+    return s;
+}
+
+// define our own memcpy to avoid SIMD instructions emitted from the compiler
+void* memcpy(void* dest, const void* src, size_t n) {
+    char* sit = (char*)src;
+    char* cdest = (char*)dest;
+    for (size_t i = 0; i < n; ++i) *cdest++ = *sit++;
+    return dest;
+}
+#endif
