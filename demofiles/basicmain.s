3550475ef4cab92f4c60eb3c95a26d9dd8dbbead
dduranro <dduranro@uwaterloo.ca>
2025-05-20 17:34:23 -0400

    basic context switch w assembly

--- demofiles/basicmain.s
+++ demofiles/basicmain.s
@@ -13,13 +13,13 @@
 main:
 	stp	x29, x30, [sp, -32]!
 	mov	x29, sp
-	ldr	x0, [x1, 8]
-	str	x19, [sp, 16]
-	mov	x19, x1
+	ldr	x0, [x1, 8]		// get first argument from program params [1]
+	str	x19, [sp, 16]   // store x19 on the stack
+	mov	x19, x1 		//faster than loading from memory
 	bl	atoi
 	mov	w1, w0
 	ldr	x0, [x19, 16]
-	mov	w19, w1
+	mov	w19, w1 //save it here
 	bl	atoi
 	mov	w1, w0
 	mov	w0, w19
@@ -28,7 +28,7 @@ main:
 	adrp	x0, .LC0
 	add	x0, x0, :lo12:.LC0
 	bl	printf
-	ldr	x19, [sp, 16]
+	ldr	x19, [sp, 16] //restore
 	mov	w0, 0
 	ldp	x29, x30, [sp], 32
 	ret
