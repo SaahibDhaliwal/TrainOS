3454aed71eb6b3b4b0c2d38104a48528760b210c
Dan Duran <dduranro@uwaterloo.ca>
2025-07-22 04:55:32 -0400

    for real this time im sorryyy

--- src/zone.cpp
+++ src/zone.cpp
@@ -461,15 +461,15 @@ void TrainReservation::initZoneB(TrackNode* trackArray) {
     zoneMap.insert(&trackArray[133], &zoneArray[31]);
 
     // ------  7 ---------
-    zoneMap.insert(&trackArray[136], &zoneArray[2]);
-    zoneMap.insert(&trackArray[137], &zoneArray[2]);
+    zoneMap.insert(&trackArray[134], &zoneArray[2]);
+    zoneMap.insert(&trackArray[135], &zoneArray[2]);
 
     // ------  9 ---------
-    zoneMap.insert(&trackArray[140], &zoneArray[4]);
-    zoneMap.insert(&trackArray[141], &zoneArray[4]);
+    zoneMap.insert(&trackArray[136], &zoneArray[4]);
+    zoneMap.insert(&trackArray[137], &zoneArray[4]);
     // ------  10 ---------
-    zoneMap.insert(&trackArray[142], &zoneArray[3]);
-    zoneMap.insert(&trackArray[143], &zoneArray[3]);
+    zoneMap.insert(&trackArray[138], &zoneArray[3]);
+    zoneMap.insert(&trackArray[139], &zoneArray[3]);
     // ------  4 ---------
     // zoneMap.insert(&trackArray[130], &zoneArray[        ]);
     // zoneMap.insert(&trackArray[131], &zoneArray[        ]);
